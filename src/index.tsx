1547a9fca9cd7491a743de85f80f6bd82108fd13
Huỳnh Công Khanh <65171477+Khanhpro1250@users.noreply.github.com>
2022-11-05 00:54:01 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+const root = document.getElementById('root');
+ReactDOM.render(
     <React.Fragment>
         <App />
     </React.Fragment>,
+    root
 );
 
 // If you want to start measuring performance in your app, pass a function
